5b01faae5218abfdb72854f19ee0cedb4171a649
Myeung Jin Lee <menggu1234@naver.com>
2023-06-10 13:24:01 +0900

    Update ros_auto_installer.sh

--- ros_auto_installer.sh
+++ ros_auto_installer.sh
@@ -192,11 +192,11 @@ case $ubuntu in
       custom_echo "Installing ROS Humble" "green"
       loading_animation
       sudo apt install curl
-      curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-      sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-
+      sudo apt update && sudo apt install curl gnupg lsb-release
+      sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+      echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null         
       sudo apt update
-      sudo apt install -y ros-desktop-full
+      sudo apt install -y ros-humble-desktop
 
       echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
       source ~/.bashrc
